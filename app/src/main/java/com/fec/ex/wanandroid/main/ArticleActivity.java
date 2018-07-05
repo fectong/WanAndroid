6342919152b6e2f8178cc2afbe7b24a6b0593176
Fe2Cu <fectong@live.com>
2018-07-06 01:23:00 +0800

    Implement Article's page with webview

--- app/src/main/java/com/fec/ex/wanandroid/main/ArticleActivity.java
+++ app/src/main/java/com/fec/ex/wanandroid/main/ArticleActivity.java
@@ -1,21 +1,54 @@
 package com.fec.ex.wanandroid.main;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.fec.ex.wanandroid.R;
 
 public class ArticleActivity extends AppCompatActivity {
 
-    private WebView mWebView;
-
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article);
+
         String URL = getIntent().getStringExtra("URL");
-        mWebView = findViewById(R.id.webView);
+        String TITLE = getIntent().getStringExtra("TITLE");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(TITLE);
+        setSupportActionBar(toolbar);
+
+        final WebView mWebView = findViewById(R.id.webView);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Snackbar.make(mWebView, description, Snackbar.LENGTH_SHORT).setAction("I SEE", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        // Do Nothing
+                    }
+                }).show();
+            }
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+        });
         mWebView.loadUrl(URL);
     }
 
